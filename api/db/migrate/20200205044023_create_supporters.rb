d60920fa232757ae86dbd9ad5b8dafe3ed9abbef
James Henderson <james@identyti.io>
2020-02-07 13:25:30 -0500

    change rails port

--- api/db/migrate/20200205044023_create_supporters.rb
+++
@@ -1,9 +0,0 @@
-class CreateSupporters < ActiveRecord::Migration[6.0]
-  def change
-    create_table :supporters do |t|
-      t.string :username
-
-      t.timestamps
-    end
-  end
-end
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Album from './template/Album';
+import './App.css';
+
+class App extends React.Component {
+
+  render () {
+    return (
+    <div className="App">
+      <Album />
+    </div>
+    );
+  }
+}
+
+export default App;
